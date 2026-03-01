a7e147909c9d431f3b1f25ad9f9a7fba2d94a5b2
mmmmtyf <menglingxuan111@qq.com>
2026-03-01 15:15:21 +0800

    修改站点信息

--- site.config.ts
+++ site.config.ts
@@ -1,10 +1,10 @@
 export const siteConfig = {
-    author: '染念',
-    title: '染念的笔记',
+    author: 'mmmmtyf',
+    title: 'mmmmtyf的笔记',
     description: 'Writing code, painful and happy',
-    keywords: "染念,染念的笔记,染念の笔记,染念的博客,博客,blog",
+    keywords: "mmmmtyf,mmmmtyf的笔记,mmmmtyfの笔记,mmmmtyf的博客,博客,blog",
     url: 'https://dyedd.cn',
-    email: '1176996982@qq.com',
+    email: '2123801583@qq.com',
     lang: 'zh-CN',
     startTime: '2017-02-11', // 添加建站时间
 }
